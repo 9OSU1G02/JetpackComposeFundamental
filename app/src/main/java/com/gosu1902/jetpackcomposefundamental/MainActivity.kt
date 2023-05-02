08c3ca447e8d7e438f7a6b48ceb3ba1876dc2009
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-05-02 12:46:40 +0700

    done

--- app/src/main/java/com/gosu1902/jetpackcomposefundamental/MainActivity.kt
+++ app/src/main/java/com/gosu1902/jetpackcomposefundamental/MainActivity.kt
@@ -3,41 +3,45 @@ package com.gosu1902.jetpackcomposefundamental
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gosu1902.jetpackcomposefundamental.ui.theme.JetpackComposeFundamentalTheme
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeFundamentalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
                 }
+                ColorBox(
+                    updateColor = { color.value = it }, modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeFundamentalTheme {
-        Greeting("Android")
-    }
+fun ColorBox(updateColor: (Color) -> Unit, modifier: Modifier = Modifier) {
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable { updateColor(Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat())) })
 }
