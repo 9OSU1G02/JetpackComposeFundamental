a9fa279a4b8a4909a605ca225f4efb92082b2047
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-05-02 11:36:24 +0700

    done

--- app/src/main/java/com/gosu1902/jetpackcomposefundamental/MainActivity.kt
+++ app/src/main/java/com/gosu1902/jetpackcomposefundamental/MainActivity.kt
@@ -3,41 +3,81 @@ package com.gosu1902.jetpackcomposefundamental
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.gosu1902.jetpackcomposefundamental.ui.theme.JetpackComposeFundamentalTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeFundamentalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+            val painter = painterResource(id = R.drawable.ic_tower)
+            val description = "Tower"
+            val title = "Tower"
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ImageCard(painter: Painter, contentDescription: String, title: String, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.fillMaxWidth(), elevation = CardDefaults.cardElevation(defaultElevation = 10.dp), shape = RoundedCornerShape(15.dp)) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(painter = painter, contentDescription = contentDescription, modifier = Modifier.fillMaxSize(), contentScale = ContentScale.Crop)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(colors = listOf(Color.Transparent, Color.Black), startY = 300f)
+                    )
+            )
+            Text(
+                text = title, modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(12.dp), fontSize = 16.sp, style = TextStyle(color = Color.White)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackComposeFundamentalTheme {
-        Greeting("Android")
+    val painter = painterResource(id = R.drawable.ic_tower)
+    val description = "Tower"
+    val title = "Tower"
+    Box(
+        modifier = Modifier
+            .height(200.dp)
+            .width(200.dp)
+    ) {
+        ImageCard(painter = painter, contentDescription = description, title = title)
     }
 }
