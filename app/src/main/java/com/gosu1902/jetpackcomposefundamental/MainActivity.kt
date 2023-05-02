e5b0e57f65dcce3bc0874613948f6ff525a127f3
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-05-02 22:31:14 +0700

    done

--- app/src/main/java/com/gosu1902/jetpackcomposefundamental/MainActivity.kt
+++ app/src/main/java/com/gosu1902/jetpackcomposefundamental/MainActivity.kt
@@ -3,41 +3,30 @@ package com.gosu1902.jetpackcomposefundamental
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gosu1902.jetpackcomposefundamental.ui.theme.JetpackComposeFundamentalTheme
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeFundamentalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            LazyColumn {
+                itemsIndexed(listOf("This", "is", "Jetpack", "Compose")) { _, string ->
+                    Text(
+                        text = string, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center, fontSize = 24.sp, modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeFundamentalTheme {
-        Greeting("Android")
     }
 }
